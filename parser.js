ce597d7bc2bfdd238520143761c0a6629e4c613a
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2017-10-23 14:59:24 +0700

    release 2

--- parser.js
+++ parser.js
@@ -1,25 +1,70 @@
 "use strict"
 
+let fs = require('fs')
+
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(id, first_name, last_name, email, phone, created_at) {
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.phone = phone
+    this.created_at = created_at
+  }
 }
 
 class PersonParser {
-
   constructor(file) {
     this._file = file
     this._people = null
+    this.inputToFile = ''
+  }
+
+  bacaFile() {
+    let data = fs.readFileSync(this._file, 'utf8').split('\n')
+    let dataPeople = []
+    for(let i = 1; i < data.length; i++) {
+      var pisahKoma = data[i].split(',')
+      dataPeople.push(new Person(pisahKoma[0], pisahKoma[1], pisahKoma[2], pisahKoma[3], pisahKoma[4], pisahKoma[5]))
+    }
+    return dataPeople
+  }
+
+  get file() {
+    return this._file
+  }
+
+  set people(dataPeople) {
+    this._people = dataPeople
   }
 
   get people() {
-    return this._people
+    let obj = {
+      data: this._people,
+      size: this._people.length-1 //karena dimulai dari index ke - 0
+    }
+    return obj
   }
 
-  addPerson() {}
+  addPerson(first_name, last_name, email, phone) {
+    this._people.push(new Person(this._people.length-1, first_name, last_name, email, phone, new Date()))
+    this.inputToFile = `${this._people.length-1},${first_name},${last_name},${email},${phone},${new Date()}\n`
+  }
+
+  save() {
+    fs.appendFileSync(this._file, this.inputToFile, 'utf8')
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
-
+// parser.bacaFile()
+parser.people = parser.bacaFile()
+parser.addPerson('Zuhri', 'Nurhuda', 'zuhri.nurhuda@gmail.com', '085258588122')
+parser.save()
+parser.addPerson('Davina', 'Bonadilla', 'davina.bonadilla@gmail.com', '085000000000')
+parser.save()
+// console.log(parser.people[200])
 console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
