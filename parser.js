1662195840be4f8d04bd4e50caed7f175f126d88
Ayunda <ayundahamzah@gmail.com>
2017-10-23 18:36:16 +0700

    belum save sama convert date

--- parser.js
+++ parser.js
@@ -1,7 +1,8 @@
 "use strict"
 const fs = require('fs')
+
 class Person {
-  construcor(id,firstname,lastname,email,phone,date){
+  constructor(id, firstname, lastname, email, phone, date){
     this.id=id;
     this.firstname=firstname;
     this.lastname=lastname;
@@ -17,61 +18,67 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
     this.data=[]
+    // this.saveString=[]
 
-  }
-  bacaFile(){
-    this._people = fs.readFileSync('./people.csv','UTF-8').split('\n')
   }
   convertData(){
-    this.bacaFile()
+    let someArr = fs.readFileSync('./people.csv','UTF-8').split('\n');
     let peopleSplit=[]
 
-
     //split isi data sesuai banyak properti
-    for(let i=0;i<this._people.length;i++){
-        this._people[i].split(',');
-      for(let j=0;j<this._people[i].length;j++){
-        peopleSplit = this._people[i].split(',');
-
-      }
+    for(let i=0;i<someArr.length;i++){
+      peopleSplit =   someArr[i].split(',');
       this.data.push(peopleSplit);
     }
-    return this;
+    // console.log(this.data);
+    for(let i=1;i<this.data.length;i++){
+      let hasil = new Person(this.data[i][0], this.data[i][1], this.data[i][2], this.data[i][3], this.data[i][4], this.data[i][5] )
+      // console.log(hasil);
+      this.people = hasil;
+      // console.log(this.people);
+      // debugger
+    }
+
   }
 
-set people(obj){
-
-    this.convertData()
-    // console.log(this.data)
-    for(let i=1;i<this.data.length;i++){
-    let obj={}
-      obj[this.data[0][0]]= this.data[i][0]
-      obj[this.data[0][1]]= this.data[i][1]
-      obj[this.data[0][2]]= this.data[i][2]
-      obj[this.data[0][3]]= this.data[i][3]
-      obj[this.data[0][4]]= this.data[i][4]
-      obj[this.data[0][5]]= this.data[i][5]
+  set people(obj){
 
     this._people.push(obj);
-    }
-}
+
+  }
 
   get people() {
-    return this._people
+    let obj={
+      dataArr:this._people,
+      size:this._people.length
+    }
+    return obj
   }
 
   addPerson(obj) {
     this.people = obj;
 
   }
-}
+
+  save(){
+
+    }
+  }
+  convertDate(){
+    // for(let i=0;i<this.people.created_atlength)
+    // let date = new Date ()
+
+  }
+
+
 
 // ;
 let parser = new PersonParser('people.csv')
-parser.addPerson(new Person('201','Mandra','Gade','Manda@gade.com','121212','2018-07-15T12:06:16-08:00'))
-console.log(parser.people)
-// console.log(parser.convertData());
-// console.log(parser.people)
+parser.convertData();
+parser.addPerson(new Person('201','Mandra','Gade','Mandra@gade.com','121212','2018-07-15T12:06:16-08:00'))
+parser.people
+console.log(parser.people);
+
 // console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
