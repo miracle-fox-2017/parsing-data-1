3b967be3e07c4e016d32d65ca661792b2ce4a95b
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-10-23 22:28:50 +0700

    TUGAS PARSING 1

--- parser.js
+++ parser.js
@@ -1,25 +1,64 @@
 "use strict"
+const fs = require('fs');
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+  constructor(id,first_name,last_name,email,phone) {
+    this.id = id;
+    this.firstname = first_name;
+    this.lastname = last_name;
+    this.email = email;
+    this.phone = phone;
+  }
 }
 
 class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = [];
+    this.arr = [];
+    this.arrayObj = [];
+  }
+
+  bacaFile() {
+    this._people = fs.readFileSync('people.csv', 'utf-8').split('\n');
+  }
+
+  dataArray() {
+    for(let i = 0 ; i < this._people.length ; i++) {
+      let pisah = this._people[i].split(',');
+      this.arr.push(pisah);
+    }
+    return this.arr;
+  }
+
+  arrayToObject() {
+    for(let i = 1 ; i < this.arr.length ; i++) {
+        this.arrayObj.push(new Person(this.arr[i][0],this.arr[i][1],this.arr[i][2],this.arr[i][3],this.arr[i][4],this.arr[i][5],this.arr[i][6]))
+    }
+    return this.arrayObj;
   }
 
   get people() {
-    return this._people
+    return this._people;
   }
 
-  addPerson() {}
+  addPerson(id,first_name,last_name,email,phone) {
+    return this.arrayObj.push(id,first_name,last_name,email,phone);
+  }
+
+  save() {
+
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
-
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// let fabio = new Person(11,'Fabio','Kounang','fabio_kosukeueki@yahoo.com','081242994000')
+// console.log(fabio);
+// console.log(parser.dataArray());
+console.log(parser.bacaFile())
+console.log(parser.dataArray());
+console.log(parser.arrayToObject());
+console.log(parser.addPerson(201,'Fabio','Kounang','fabio_kosukeueki@yahoo.com','081242994000'));
