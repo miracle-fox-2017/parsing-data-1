4736293da8b0184823d3df75c817e1f1b014590f
riskinputra <riski.n.putra@gmail.com>
2017-10-23 17:29:37 +0700

    release-0

--- parser.js
+++ parser.js
@@ -1,8 +1,19 @@
 "use strict"
 
+
+let fs = require('fs');
+
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(id, firstName, lastName, email, phone, createAt){
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+    this.createAt = createAt;
+  }
 }
 
 class PersonParser {
@@ -13,13 +24,37 @@ class PersonParser {
   }
 
   get people() {
-    return this._people
+    this._people = fs.readFileSync(this._file).toString().split('\n');
+    let obj = {
+        data: this._people,
+        size: this._people.length-1,
+     }
+
+    return obj;
   }
 
-  addPerson() {}
+  getNewObj(){
+    this._people = fs.readFileSync(this._file).toString().split('\n');
+    let newPeople = [];
 
+    for(let i = 1; i < this._people.length; i++) {
+      var newArrPeople = this._people[i].split(',')
+      console.log(newArrPeople);
+      newPeople.push(new Person(newArrPeople[0], newArrPeople[1], newArrPeople[2], newArrPeople[3],newArrPeople[4],newArrPeople[5] ))
+    }
+    return newPeople;
+  }
+
+
+  addPerson(firstName, lastName, email, phone, createAt) {
+    this._people.push(this._people.length-1, firstName, lastName, email, phone, createAt)
+  }
 }
 
 let parser = new PersonParser('people.csv')
+
+console.log(parser.getNewObj());
+
+parser.addPerson('riski','n','riski@gmail.com','0990890842',new Date())
 
 console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
